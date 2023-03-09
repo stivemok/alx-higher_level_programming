3f3120f8efd5884765889dcc93a52edf7760ceb6
stivemok <stivemok@gmail.com>
2023-03-09 02:01:44 -0800

    ininite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     num = 0
-    for i in range(len(argv) -1):
+    for i in range(len(argv) - 1):
         num += int(argv[i + 1])
     print("{}".format(num))
