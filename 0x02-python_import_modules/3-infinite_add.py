64e92f1ffaffaedc8073f010cefe2b8caa93762d
stivemok <stivemok@gmail.com>
2023-03-09 01:50:34 -0800

    readme

--- 0x02-python_import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    num = 0
-    for i in range(len(argv) -1):
-        num += int(argv[i + 1])
-    print("{}".format(num))
