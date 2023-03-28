04f1e3968555f55afc0ec98a0cbf5910181a6b8f
stivemok <stivemok@gmail.com>
2023-03-28 11:34:48 -0700

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Documentation a class Square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """Initializes class attribute
+
+        Args:
+        size: size of the square"""
+
+        self.__size = size
