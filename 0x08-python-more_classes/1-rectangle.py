38f58f4d5f7484f1e895fbbe55ca951c2bd7aa9f
stivemok <stivemok@gmail.com>
2023-04-04 04:04:29 -0700

    how many instance

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,6 @@ class Rectangle:
         width: width of rectangle
         height: height of rectangle
         """
-
         self.height = height
         self.width = width
 
@@ -24,7 +23,7 @@ class Rectangle:
             if type(value) is not int:
                 raise TypeError("width must be an integer")
             if value < 0:
-                raise ValueError("height must be >= 0")
+                raise ValueError("width must be >= 0")
             self.__width = value
 
         @property
---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" Rectangle module """
+
+
+class Rectangle:
+    """ Rectangle class """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integet")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle = []
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle.append('#')
+            if i is not self.__height - 1:
+                rectangle.append('\n')
+        return ''.join(rectangle)
+
+    def __repr__(self):
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
