0240ccf8713a0423dde662545a387feed819a9b5
stivemok <stivemok@gmail.com>
2023-04-04 09:04:47 -0700

    A square is a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """ Rectangle class """
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
