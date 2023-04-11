0c8f0e803c6db5dc386ebf6c4d9c2760ac8ac034
stivemok <stivemok@gmail.com>
2023-04-11 01:34:16 -0700

    Squaare #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -41,6 +41,6 @@ class Square(Rectangle):
         self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
-        
+
     def area(self):
         return self.__size ** 2
