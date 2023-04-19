3edc0a82250d645f5988f81fd851569a1b14033c
stivemok <stivemok@gmail.com>
2023-04-19 06:54:11 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,6 +8,18 @@ from models.base import Base
 
 class TestBaseClass(unittest.TestCase):
     """unit test of base class"""
+    def test_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
+
     def test_documentation(self):
         self.assertTrue(len(base.__doc__) > 0)
 
