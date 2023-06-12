852ed6b82008fe17de699a524f077733c869811b
stivemok <stivemok@gmail.com>
2023-06-12 08:26:54 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const arg = process.argv.length;
+if (arg === 2)
+{
+    console.log('No argument');
+}
+else if (arg === 3)
+{
+    console.log('Argument found');
+}
+else
+{
+    console.log('Arguments found');
+}
