61e512a5ef04d050ae87067183c4e1fc31ecae6b
stivemok <stivemok@gmail.com>
2023-06-30 09:45:43 -0700

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the URL
+and displays the body of the response"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    resp = requests.get(sys.argv[1])
+    if resp.status_code >= 400:
+        print("Error code: {}".format(resp.status_code))
+    else:
+        print(resp.text)
