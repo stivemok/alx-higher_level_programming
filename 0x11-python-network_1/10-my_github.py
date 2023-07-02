ad755037798498a623bf285de87372097ad6199c
stivemok <stivemok@gmail.com>
2023-07-01 23:47:01 -0700

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,6 +7,6 @@ import requests
 
 if __name__ == '__main__':
 resp = requests.get('https://api.github.com/user',
-                    auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+                    auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
 json = resp.json()
 print('{}'.format(json.get('id')))
