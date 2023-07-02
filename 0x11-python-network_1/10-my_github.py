6f4859a29877ae09b82d0600d7ac1ffa1c7d0100
stivemok <stivemok@gmail.com>
2023-07-01 23:37:26 -0700

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,7 @@ and uses the GitHub API to display your id"""
 import sys
 import requests
 
+if __name__ == '__main__':
 resp = requests.get('https://api.github.com/user',
                     auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
 json = resp.json()
