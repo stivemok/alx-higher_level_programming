5ce08badcd3542ddd93374e4e76ca302e0e83cc4
stivemok <stivemok@gmail.com>
2023-08-03 06:21:16 -0700

    Click and turn red

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
 // JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header
-$('DIV#red_header').click(finction(){
+$('DIV#red_header').click(function(){
     $('header').css({ color: '#FF0000' });
 });
